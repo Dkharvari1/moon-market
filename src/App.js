cb401c448d652ef480f0647c90d5161bcfbf98a7
Dev Kharvari <devkharvari@gmail.com>
2024-02-09 20:35:28 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import login from './pages/login'
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <login></login>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+ 
+
+export default App;
